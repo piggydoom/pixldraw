1d8fd31cb5c0b852e5bee57caabd8ce1be24f17f
piggydoom <artistpiggy@gmail.com>
2026-04-01 15:56:54 +1100

    grid added now working on download png

--- src/main/java/com/piggydoom/Controller.java
+++ src/main/java/com/piggydoom/Controller.java
@@ -6,14 +6,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
+// import java.awt.image.RenderedImage;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+// import javafx.embed.swing.SwingFXUtils;
 
 public class Controller {
 
@@ -22,28 +28,73 @@ public class Controller {
 
     @FXML
     public Canvas canvas;
-    
+
+    @FXML
+    public Canvas gridCanvas;
+
+    @FXML
+    public ColorPicker gridColourSetting;
+
+    @FXML
+    public CheckMenuItem gridDisplaySetting;
+
     @FXML
     public ColorPicker colourSel;
-    
+
     GraphicsContext ctx;
-    double x = 0; 
+    GraphicsContext ctxGrid;
+    double x = 0;
     double y = 0;
     boolean mouseDown = false;
+
+    public void toggleGCSdisp() {
+        if (gridColourSetting.isVisible()) {
+            gridColourSetting.hide();
+        } else {
+            gridColourSetting.show();
+        }
+    }
+
+    public void downloadCanvas(){
+        // WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+        // canvas.snapshot(null, writableImage);
+        // RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+        System.out.println(System.getProperty("javafx.runtime.version"));
+
+    }
+
+    public void updateGrid() {
+        ctxGrid.setFill(Color.WHITE);
+        ctxGrid.fillRect(0, 0, 500, 500);
+
+        if (gridDisplaySetting.isSelected()) {
+            ctxGrid.setStroke(gridColourSetting.getValue());
+            ctxGrid.setLineWidth(2);
+            for (int gridYhop = 0; gridYhop < 510; gridYhop += 10) {
+                ctxGrid.strokeLine(0, gridYhop, 500, gridYhop);
+            }
+            for (int gridXhop = 0; gridXhop < 510; gridXhop += 10) {
+                ctxGrid.strokeLine(gridXhop, 0, gridXhop, 500);
+            }
+        } else {
+            ctxGrid.setFill(Color.WHITE);
+            ctxGrid.fillRect(0, 0, 500, 500);
+        }
+    }
 
     @FXML
     public void initialize() {
         ctx = canvas.getGraphicsContext2D();
-        ctx.setFill(Color.WHITE);
-        ctx.fillRect(0, 0,  500, 500);
-        ctx.setFill(Color.BLACK);
-        }
+        ctxGrid = gridCanvas.getGraphicsContext2D();
+        gridColourSetting.setValue(Color.BLACK);
+        updateGrid();
+    }
 
     @FXML
-    public void updateColor(){
-    Color selectedColor = colourSel.getValue();
-    ctx.setFill(selectedColor);
-    System.out.println(selectedColor);
+    public void updateColor() {
+        Color selectedColor = colourSel.getValue();
+        ctx.setFill(selectedColor);
+        System.out.println(selectedColor);
     }
 
     @FXML
@@ -52,4 +103,7 @@ public class Controller {
         y = Math.floor(event.getY() / 10) * 10;
         ctx.fillRect(x, y, 10, 10);
     }
+
+    
+
 }
